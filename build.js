537b7d0ac12c6b17b65ddbcd1068789b743bc35c
azu <azuciao@gmail.com>
2015-04-02 21:57:15 +0900

    imp

--- build.js
+++ build.js
@@ -1,295 +1,162 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-/**
- * Created by azu on 2015/03/22.
- */
-var OptionT = require('option-t');
+// LICENSE : MIT
+"use strict";
+// MIT
+var SyncPromise = require('sync-promise');
+var getSelectedRadios = require("./utils/radio-util").getSelectedRadios;
 function getRadioState() {
-    var select = document.getElementById("js-select");
-    if (select.selectedIndex === -1) {
-        return new OptionT.None();
+    var selected = getSelectedRadios();
+    if (selected.length === 0) {
+        return new SyncPromise(function (resolve, reject) {
+            reject(new Error("選択されてない"));
+        });
     }
-    return new OptionT.Some(select.selectedIndex);
+    var s = selected[0];
+    return new SyncPromise(function (resolve) {
+        resolve(s);
+    });
 }
 
 
 document.getElementById("js-output").addEventListener("click", function () {
-    var option = getRadioState();
-    if(option.isSome) {
-        console.log(option.unwrap())
-    }else{
-        console.log("None");
+    var promise = getRadioState();
+    promise.then(function (value) {
+        console.log(value);
+    }).catch(function (error) {
+        console.error(error);
+    });
+});
+},{"./utils/radio-util":2,"sync-promise":3}],2:[function(require,module,exports){
+// LICENSE : MIT
+"use strict";
+function getSelectedRadios() {
+    var results = [];
+    var radios = document.querySelectorAll('#js-radio-fieldset input[name="judge"]');
+    for (var i = 0; i < radios.length; i++) {
+        var radio = radios[i];
+        if (radio.checked) {
+            results.push(radio);
+        }
     }
-});
-},{"option-t":3}],2:[function(require,module,exports){
-/**
- * MIT License
- *
- * Copyright (c) 2015 Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
-'use strict';
-
-/**
- *  @constructor
- *  @template   T
- *
- *  A base object of `Option<T>`.
- *  This is only used to `option instanceof OptionT`
- *  in an language environment which does not have an interface type system.
- *
- *  The usecase example is a `React.PropTypes.
- */
-var OptionT = function OptionTBase() {};
-OptionT.prototype = Object.freeze({
-    /**
-     *  Return whether this is `Some<T>` or not.
-     *
-     *  @return {boolean}
-     */
-    get isSome() {
-        return this.is_some;
-    },
-
-    /**
-     *  Returns the inner `T` of a `Some<T>`.
-     *
-     *  @template   T
-     *
-     *  @return {T}
-     *  @throws {Error}
-     *      Throws if the self value equals `None`.
-     */
-    unwrap: function OptionTUnwrap() {
-        if (!this.is_some) {
-            throw new Error('called `unwrap()` on a `None` value');
-        }
-
-        return this.value;
-    },
-
-    /**
-     *  Maps an `Option<T>` to `Option<U>` by applying a function to a contained value.
-     *
-     *  @template   T, U
-     *
-     *  @param  {function(T):U}    fn
-     *  @return {!Option<U>}
-     */
-    map: function OptionTMap(fn) {
-        if (!this.is_some) {
-            // cheat to escape from a needless allocation.
-            return this;
-        }
-
-        var value = fn(this.value);
-        var option = new Some(value);
-        return option;
-    },
-
-    /**
-     *  Returns `None` if the self is `None`,
-     *  otherwise calls `fn` with the wrapped value and returns the result.
-     *
-     *  @template   T, U
-     *
-     *  @param  {function(T): !Option<U>}    fn
-     *  @return {!Option<U>}
-     */
-    flatMap: function OptionTFlatMap(fn) {
-        if (!this.is_some) {
-            // cheat to escape from a needless allocation.
-            return this;
-        }
-
-        var mapped = fn(this.value);
-        var isOption = (mapped instanceof OptionT);
-        if (!isOption) {
-            throw new Error('Option<T>.flatMap()\' param `fn` should return `Option<T>`.');
-        }
-
-        return mapped;
-    },
-
-    /**
-     *  The alias of `Option<T>.flatMap()`.
-     *
-     *  @template   T, U
-     *
-     *  @param  {function(T): !Option<U>}    fn
-     *  @return {!Option<U>}
-     */
-    andThen: function OptionTAndThen(fn) {
-        return this.flatMap(fn);
-    },
-
-    /**
-     *  Returns `None` if the self is `None`, otherwise returns `optb`.
-     *
-     *  @template   U
-     *
-     *  @param  {!Option<U>} optb
-     *  @return {!Option<U>}
-     */
-    and: function OptionTAnd(optb) {
-        return this.is_some ? optb : this;
-    },
-
-    /**
-     *  Returns the self if it contains a value, otherwise returns `optb`.
-     *
-     *  @template   T
-     *
-     *  @param  {!Option<T>} optb
-     *  @return {!Option<T>}
-     */
-    or: function OptionTOr(optb) {
-        return this.is_some ? this : optb;
-    },
-
-    /**
-     *  Returns the self if it contains a value,
-     *  otherwise calls `fn` and returns the result.
-     *
-     *  @template   T
-     *
-     *  @param  {function(): !Option<T>} fn
-     *  @return {!Option<T>}
-     */
-    orElse: function OptionTOr(fn) {
-        if (this.is_some) {
-            return this;
-        }
-        else {
-            var value = fn();
-            if (value instanceof OptionT) {
-                return value;
-            }
-
-            throw new Error('Option<T>.orElse()\' param `fn` should return `Option<T>`.');
-        }
-    },
-
-    /**
-     *  Finalize the self.
-     *  After this is called, the object's behavior is not defined.
-     *
-     *  @return {void}
-     */
-    drop: function OptionTDrop() {
-        this.value = null;
-        Object.freeze(this);
-    },
-});
-
-var OptionPrototype = new OptionT();
-
-/**
- *  @constructor
- *  @template   T
- *  @extends    {OptionT<T>}
- *
- *  @param  {T}   val
- */
-var Some = function OptionTSome(val) {
-    /* eslint-disable camelcase */
-    /**
-     *  @private
-     *  @type   {boolean}
-     */
-    this.is_some = true;
-    /* eslint-enable */
-
-    /**
-     *  @private
-     *  @type   {T}
-     */
-    this.value = val;
-    Object.seal(this);
-};
-Some.prototype = OptionPrototype;
-
-/**
- *  @constructor
- *  @template   T
- *  @extends    {OptionT<T>}
- */
-var None = function OptionTNone() {
-    /* eslint-disable camelcase */
-    /**
-     *  @private
-     *  @type   {boolean}
-     */
-    this.is_some = false;
-    /* eslint-enable */
-
-    /**
-     *  @private
-     *  @type   {T}
-     */
-    this.value = undefined;
-    Object.seal(this);
-};
-None.prototype = OptionPrototype;
-
+    return results;
+}
 module.exports = {
-    Some: Some,
-    None: None,
-    OptionT: OptionT,
+    getSelectedRadios: getSelectedRadios
 };
-
 },{}],3:[function(require,module,exports){
-/**
- * @license MIT License
- *
- * Copyright (c) 2015 Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
+function isPromise(p) {
+  return p && typeof p.then === 'function';
+}
 
-'use strict';
+// States
+var PENDING = 2,
+    FULFILLED = 0, // We later abuse these as array indices
+    REJECTED = 1;
 
-var Option = require('./OptionT');
+function SyncPromise(fn) {
+  var self = this;
+  self.v = 0; // Value, this will be set to either a resolved value or rejected reason
+  self.s = PENDING; // State of the promise
+  self.c = [[],[]]; // Callbacks c[0] is fulfillment and c[1] is rejection callbacks
+  self.a = false; // Has the promise been resolved synchronously
+  var syncResolved = true;
+  function transist(val, state) {
+    self.a = syncResolved;
+    self.v = val;
+    self.s = state;
+    if (state === REJECTED && !self.c[state].length) {
+      throw val;
+    }
+    self.c[state].forEach(function(fn) { fn(val); });
+    self.c = null; // Release memory.
+  }
+  function resolve(val) {
+    if (!self.c) {
+      // Already resolved, do nothing.
+    } else if (isPromise(val)) {
+      val.then(resolve).catch(reject);
+    } else {
+      transist(val, FULFILLED);
+    }
+  }
+  function reject(reason) {
+    if (!self.c) {
+      // Already resolved, do nothing.
+    } else if (isPromise(reason)) {
+      reason.then(reject).catch(reject);
+    } else {
+      transist(reason, REJECTED);
+    }
+  }
+  fn(resolve, reject);
+  syncResolved = false;
+}
 
-var OptionT = {
-    Some: Option.Some,
-    None: Option.None,
-    OptionT: Option.OptionT,
+var prot = SyncPromise.prototype;
+
+prot.then = function(cb) {
+  var self = this;
+  if (self.a) { // Promise has been resolved synchronously
+    throw new Error('Can not call then on synchonously resolved promise');
+  }
+  return new SyncPromise(function(resolve, reject) {
+    function settle() {
+      try {
+        resolve(cb(self.v));
+      } catch(e) {
+        reject(e);
+      }
+    }
+    if (self.s === FULFILLED) {
+      settle();
+    } else if (self.s === REJECTED) {
+      reject(self.v);
+    } else {
+      self.c[FULFILLED].push(settle);
+      self.c[REJECTED].push(reject);
+    }
+  });
 };
 
-module.exports = OptionT;
+prot.catch = function(cb) {
+  var self = this;
+  if (self.a) { // Promise has been resolved synchronously
+    throw new Error('Can not call catch on synchonously resolved promise');
+  }
+  return new SyncPromise(function(resolve, reject) {
+    function settle() {
+      try {
+        resolve(cb(self.v));
+      } catch(e) {
+        reject(e);
+      }
+    }
+    if (self.s === REJECTED) {
+      settle();
+    } else if (self.s === FULFILLED) {
+      resolve(self.v);
+    } else {
+      self.c[REJECTED].push(settle);
+      self.c[FULFILLED].push(resolve);
+    }
+  });
+};
 
-},{"./OptionT":2}]},{},[1]);
+SyncPromise.all = function(promises) {
+  return new SyncPromise(function(resolve, reject, l) {
+    l = promises.length;
+    promises.forEach(function(p, i) {
+      if (isPromise(p)) {
+        p.then(function(res) {
+          promises[i] = res;
+          --l || resolve(promises);
+        }).catch(reject);
+      } else {
+        --l || resolve(promises);
+      }
+    });
+  });
+};
+module.exports = SyncPromise;
+
+},{}]},{},[1]);
